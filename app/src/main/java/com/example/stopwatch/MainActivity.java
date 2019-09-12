e326d22d910f5902fd9af83947041aa32dbc1d0a
Timothy Johnson <timothyjohnsontimothy@gmail.com>
2019-09-12 13:35:20 -0700

    Fixed rotation bug

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.stopwatch;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-
 public class MainActivity extends AppCompatActivity {
     private Button startStop;
     private Button reset;
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isOn;
     private long lastNumber;
     private ConstraintLayout constraintLayout;
+    public static final String KEY_CHRONOMETER_BASE = "chronometer base";
     private void wireWidgets() {
         startStop = findViewById(R.id.button_main_start_stop);
         reset = findViewById(R.id.button_main_reset);
@@ -52,14 +53,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
     public static final String TAG = MainActivity.class.getSimpleName();
-
     //  Look up the Log class for Android
     //  List all the levels of logging and when they're used
     //  Verbose - Debug - Info - Warning - Error - Assert
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
         int bl = (int)(Math.random() * 256);
         int color = Color.rgb(r, g, bl);
         constraintLayout.setBackgroundColor(color);
+
+        //  if the savedInstanceState isn't null
+        //  pull out the value of the base that we saved from the bundle
+        //  set the chronometer's base to that value
+        //  start the chronometer
+
+        //  next functionality would be to also store in the bundle whether it was running or stopped
+        //  to decide to start it or not in onCreate
+        if(savedInstanceState != null){
+            stopWatch.setBase(savedInstanceState.getLong(KEY_CHRONOMETER_BASE));
+            stopWatch.start();
+        }
     }
     @Override
     protected void onStart() {
@@ -84,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
+
     }
     @Override
     protected void onResume() {
@@ -95,19 +105,21 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.d(TAG, "onRestart: ");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop: ");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
-
     }
 
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(KEY_CHRONOMETER_BASE, stopWatch.getBase());
+    }
 }
